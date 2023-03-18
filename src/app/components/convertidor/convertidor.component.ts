57fe987cd937b8cb18176575d8dc5630ebcb0535
Fernando Castellanos <fernan2cas96@gmail.com>
2023-03-18 14:56:04 -0400

    first commit

---
+++ src/app/components/convertidor/convertidor.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-convertidor',
+  templateUrl: './convertidor.component.html',
+  styleUrls: ['./convertidor.component.css']
+})
+export class ConvertidorComponent implements OnInit {
+  cantidad = 0;
+  tengo = 'USD';
+  quiero = 'EUR';
+  total = 0;
+
+  monedas: string[] = ['USD', 'EUR', 'LIBRA', 'MLC'];
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  convertir(){
+    switch(this.tengo){
+      case 'USD':
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad;
+        }
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad * 0.84;
+        }
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 0.75;
+        }
+        if(this.quiero === 'MLC'){
+          this.total = this.cantidad * 1.1;
+        }
+        break;
+      case 'EUR':
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad * 1.20;
+        }
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad;
+        }
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 0.9;
+        }
+        if(this.quiero === 'MLC'){
+          this.total = this.cantidad * 1.3;
+        }
+        break;
+      case 'LIBRA':
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad * 1.33;
+        }
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad * 1.11;
+        }
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad;
+        }
+        if(this.quiero === 'MLC'){
+          this.total = this.cantidad * 1.35;
+        }
+        break;
+      case 'MLC':
+        if(this.quiero === 'USD'){
+          this.total = this.cantidad * 1.1;
+        }
+        if(this.quiero === 'EUR'){
+          this.total = this.cantidad * 1.2;
+        }
+        if(this.quiero === 'LIBRA'){
+          this.total = this.cantidad * 3;
+        }
+        if(this.quiero === 'MLC'){
+          this.total = this.cantidad;
+        }
+        break;
+
+    }
+  }
+}
